2689e527d2ecb9f5006ec62cc89379b8f76bb69f
David Amaya Hernandez <amayadavid885@gmail.com>
2025-11-02 12:11:43 -0500

    stage 5 error fixes

--- scripts/formSubmit.js
+++ scripts/formSubmit.js
@@ -1,49 +1,47 @@
 //edit profile selectors
 const editProfileForm = document.querySelector("#edit-profile-form");
 const formName = editProfileModal.querySelector("#profile-name-input");
-const formDescription = editProfileModal.querySelector("#profile-description-input");
+const formDescription = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-formName.value = profileName.textContent;
-formDescription.value = profileDescription.textContent;
 
 //new post selectors
 const newPostForm = document.querySelector("#new-post-form");
 const formImageURL = newPostModal.querySelector("#post-url-input");
 const formCaption = newPostModal.querySelector("#post-caption-input");
 
-
 //Profile form submit handler
 function editProfileHandler(e) {
-    e.preventDefault();
+  formName.value = profileName.textContent;
+  formDescription.value = profileDescription.textContent;
+  e.preventDefault();
 
-    profileName.textContent = formName.value;
-    profileDescription.textContent = formDescription.value;
+  profileName.textContent = formName.value;
+  profileDescription.textContent = formDescription.value;
 
-    editProfileModal.classList.remove("modal_is_opened")
+  editProfileModal.classList.remove("modal_is_opened");
 
-    e.target.reset();
-    
-    formName.value = profileName.textContent;
-    formDescription.value = profileDescription.textContent;
+  e.target.reset();
 
+  formName.value = profileName.textContent;
+  formDescription.value = profileDescription.textContent;
 }
 editProfileForm.addEventListener("submit", (e) => {
-    editProfileHandler(e);
-})
+  editProfileHandler(e);
+});
 
 //New Post form submit handler
 function newPostHandler(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    console.log(formImageURL.value + ", " + formCaption.value);
+  console.log(formImageURL.value + ", " + formCaption.value);
 
-    newPostModal.classList.remove("modal_is_opened")
+  newPostModal.classList.remove("modal_is_opened");
 
-    e.target.reset();
-
+  e.target.reset();
 }
 newPostForm.addEventListener("submit", (e) => {
-    newPostHandler(e);
-})
-
+  newPostHandler(e);
+});
--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,37 @@ const newPost = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostClose = newPostModal.querySelector(".modal__close-btn");
 
+//open and close modal
+function openModal(e) {
+  if (e.target.classList.contains("profile__edit-button")) {
+    editProfileModal.classList.add("modal_is_opened");
+  } else if (e.target.classList.contains("profile__add-button")) {
+    newPostModal.classList.add("modal_is_opened");
+  }
+}
+
+function closeModal(e) {
+  if (e.target.classList.contains("profile__edit-button")) {
+    editProfileModal.classList.remove("modal_is_opened");
+  } else if (e.target.classList.contains("profile__add-button")) {
+    newPostModal.classList.remove("modal_is_opened");
+  }
+}
+
 //open and close edit profile modal
 editProfile.addEventListener("click", (e) => {
-    editProfileModal.classList.add("modal_is_opened")
-})
+  openModal(e);
+});
 
 editProfileClose.addEventListener("click", (e) => {
-    editProfileModal.classList.remove("modal_is_opened")
-})
+  closeModal(e);
+});
 
 //open and close new post modal
 newPost.addEventListener("click", (e) => {
-    newPostModal.classList.add("modal_is_opened")
-})
+  openModal(e);
+});
 
 newPostClose.addEventListener("click", (e) => {
-    newPostModal.classList.remove("modal_is_opened")
-})
+  closeModal(e);
+});
